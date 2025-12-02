0aee60a4074260c31a903c0107d1ee22f7fedcc3
Hugo Vieira <hgovra@gmail.com>
2025-12-02 19:48:52 -0300

    Configura CI/CD.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: "https://avsystemgeo-avs.github.io/axia-sigo-manual/",
+  url: "https://avsystemgeo-avs.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/axia-sigo-manual/",
